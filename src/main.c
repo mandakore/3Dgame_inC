766cd2c89c4ef0944307f9afb1616ba01e8682c7
mandakore <galaxyfish11@gmail.com>
2025-10-04 19:25:51 +0900

    loopを実装

--- src/main.c
+++ src/main.c
@@ -6,18 +6,60 @@
 /*   By: atashiro <atashiro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/04 18:12:08 by atashiro          #+#    #+#             */
-/*   Updated: 2025/10/04 18:30:03 by atashiro         ###   ########.fr       */
+/*   Updated: 2025/10/04 19:15:25 by atashiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3D.h"
 
-int	main(void)
-{
-	void	*mlx_ptr;
-	void	*win_ptr;
 
-	mlx_ptr = mlx_init();
-	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "cub3D"); //可変？
-	mlx_loop(mlx_ptr);
+void	free_all(t_game game, char **map, size_t i)
+{
+	while (i > 0)
+		free(map[--i]);
+	free(map);
+	mlx_clear_window(game.mlx_ptr, game.win_ptr);
+	mlx_destroy_window(game.mlx_ptr, game.win_ptr);
+	mlx_destroy_display(game.mlx_ptr);
+	free(game.mlx_ptr);
+}
+
+int	close_window(t_game *game)
+{
+	mlx_loop_end(game->mlx_ptr);
+	return (0);
+}
+
+int	handle_keypress(int keysym, t_game *game)
+{
+
+	if (keysym == XK_Escape)
+	{
+		close_window(game);
+		return (0);
+	}
+	//キーハンドラを記述
+	return (0);
+}
+
+void	start_game_loop(t_game *game)
+{
+	mlx_hook(game->win_ptr, E_KEY_PRESS, M_KEY_PRESS, handle_keypress, game);
+	mlx_hook(game->win_ptr, E_WIN_CLOSE, M_WIN_RESIZE, close_window, game);
+	// mlx_hook(game->win_ptr, E_WIN_RESIZE, M_WIN_CLOSE, minimize_window, game);
+	mlx_loop(game->mlx_ptr);
+	free_all(*game, game->map.map, game->map.height);
+}
+
+int	main(int argc, char **argv)
+{
+
+	(void) argc;
+	(void) argv;
+
+	t_game	game;
+
+	game.mlx_ptr = mlx_init();
+	game.win_ptr = mlx_new_window(game.mlx_ptr, 800, 600, "cub3D"); //可変？
+	start_game_loop(&game);
 }
